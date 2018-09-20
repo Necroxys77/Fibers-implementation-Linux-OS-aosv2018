508d31497d5255084f4608a8e84b4ea7080f5fab
Ubuntu_17_10 <matteo_mariani94@yahoo.com>
2018-09-20 19:14:33 +0200

    Added comments #1

--- code/fibermodule.h
+++ code/fibermodule.h
@@ -41,19 +41,26 @@ struct pt_regs {
 #include <linux/device.h>         // Header to support the kernel Driver Model
 #include <linux/kernel.h>         // Contains types, macros, functions for the kernel
 #include <linux/fs.h>             // Header for the Linux file system support
-#include <linux/uaccess.h> // Required for the copy to user function
+#include <linux/uaccess.h>      // Required for the copy to user function
 #include <linux/sched/task_stack.h>
-#include <linux/hashtable.h> //Required for using linux implementation of hashtables 
+#include <linux/hashtable.h>    //Required for using linux implementation of hashtables 
 #include <linux/slab.h>
-#include <asm/atomic.h>
+#include <asm/atomic.h>     //Required for using safe concurrency control
 
 #define MAGIC 'a'
 #define convertF _IO(MAGIC, 0)
 #define createF _IO(MAGIC, 1)
 #define switchF _IO(MAGIC, 2)
 
-#define STACK_SIZE (4096*2)
+#define STACK_SIZE (4096*2)  // 8KB
 
+
+/**
+ * Struct that defines a fiber in the system
+ * 
+ * @field   struct pt_regs *regs describes the registers associated to a fiber 
+ * @field   int fiber_id is the identifier of the fiber inside a specific thread
+ */
 struct fiber {
     struct pt_regs *regs;
     int fiber_id;
@@ -61,6 +68,15 @@ struct fiber {
     void *param; //unused!
 };
 
+/**
+ * Struct that handle the parameters that are passed from/to user space and kernel space through ioctl call
+ * 
+ * @field   void **args is the address in which (???)
+ * @field   unsigned long *sp is the pointer to the stack pointer
+ * @field   unsigned long *bp is the pointer to the base pointer
+ * @field   int fiber_id is the id of the fiber that is filled in order to give infos to userspace
+ *          from kernel space
+ */
 struct ioctl_params {
     void **args;
     unsigned long *sp, *bp;
@@ -73,6 +89,14 @@ struct creator_thread {
     struct table_element_thread *creator_thread;
 };
 
+/**
+ * Struct that describe an element in the hashtable threads that contains the threads that have at least one fiber.
+ * An element of the hashtable is associated to one thread - that is the creator fiber
+ * 
+ * @field   atomic_t total fibers is the number of fibers part of the thread pid_t parent
+ * @field   pid_t parent is the pid of the thread that is now a fiber
+ * @field   struct fiber fiber
+ */
 struct table_element_thread {
     atomic_t total_fibers;
     pid_t parent;
@@ -94,6 +118,14 @@ struct table_element_fiber {
 };
 */
 
+/**
+ * Method description.
+ * @param param1 param1 description 
+ * @param param2 param2 description
+ * @return return description
+ * @since date since method is created
+ * @author who have made this method.
+ */
 static long my_ioctl(struct file *, unsigned int, unsigned long);
 static char *unlock_sudo(struct device *, umode_t *);
 static int __init starting(void);
